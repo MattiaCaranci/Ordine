01acc3569c7488f75c74b92f4a87ec332b2025e8
sieg-feranil <127198356+sieg-feranil@users.noreply.github.com>
2024-01-12 11:34:48 +0100

    boh

---
+++ Client/commonClasses/classes.ts
@@ -0,0 +1,41 @@
+export interface Product {
+  code: number;
+  name: string;
+  variants: Array<string>;
+  description: string;
+  price: number;
+  retailer: string;
+}
+
+export interface Order {
+  orderNumer?: number;
+  orderDate?: Date;
+  totalAmount: number;
+  orderLineItems: Array<OrderLineItems>;
+  customer: Customer;
+}
+export interface Address {
+  id?: number;
+  street: string;
+  city: string;
+  state: string;
+  postalCode: number;
+}
+export interface Customer {
+  customerId?: number;
+  firstname: string;
+  lastname: string;
+  age: number;
+  email: string;
+  phone: string;
+  locale: string;
+  billingAddress: Address;
+  shippingAddress?: Address;
+}
+export interface OrderLineItems {
+  id?: number;
+  order?: Order;
+  product: Product;
+  amount: number;
+  quantity: number;
+}
